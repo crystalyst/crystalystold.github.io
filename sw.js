8f381cca0f79f72bebbfb4d27254c85bf0d9f237
crystalyst <jy2908@columbia.edu>
2020-07-28 16:37:33 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-61b2943480fed3674b7e.js"
+    "url": "webpack-runtime-3ad6d83b2e0a12afb314.js"
   },
   {
     "url": "styles.69c8303230227041b58c.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7d8cb274719d6448c3df4bdc6728718d"
+    "revision": "8dcb13990a5eb9f1084e4f57109990cb"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c9875e2473a7c518755f.js"
